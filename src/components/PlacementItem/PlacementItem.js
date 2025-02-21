8073d7b942f7adde7a929e57cb4bed9b2a45385b
nguyen tung <tnkoko123@gmail.com>
2025-02-21 10:28:12 +0700

    90% trips - transports - hotels

--- src/components/PlacementItem/PlacementItem.js
+++ src/components/PlacementItem/PlacementItem.js
@@ -1,11 +1,21 @@
 import classNames from "classnames/bind";
 import styles from './PlacementItem.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as emptyStar} from "@fortawesome/free-regular-svg-icons";
 import { Link } from "react-router-dom";
 import Button from "../Button";
+import { faStarHalfStroke,faStar,faPersonChalkboard,faShop,faMapLocationDot,faGuitar,faBurger,faMugHot, faCampground, faExclamation, faPersonSwimming, faFish } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+
 
 const cx = classNames.bind(styles)
 
 function PlacementItem({type, data}) {
+    useEffect(() => {
+    console.log("Data received:", data);
+}, [data]);
+    const defaultActivities = ["souvenir", "visit", "campfire","music","food","drink","travel",'swim','fishing'];
+
     const contentMap = {
         trips: {
             name : data.name,
@@ -14,6 +24,7 @@ function PlacementItem({type, data}) {
             reviews : data.reviews,
             img : data.img,
             description : data.description,
+            activities : data.activities ? data.activities.split(',') : []
         },
 
         hotels: {
@@ -31,7 +42,41 @@ function PlacementItem({type, data}) {
         }
     }
     const content = contentMap[type] || {}
+    
+    const StarRating = ({rating}) => {
+        const stars = []
+        const roundedRating = Math.round(rating * 2) / 2 
+        for(let i=0;i<5;i++){
+            if(i < Math.floor(roundedRating)){
+                stars.push(<FontAwesomeIcon icon={faStar} color="#FFD700" />)
+            }
+            else if(i + 0.5 === roundedRating) {
+                stars.push(<FontAwesomeIcon icon={faStarHalfStroke} color="#FFD700"/>)
+            }
+            else {
+                stars.push(<FontAwesomeIcon icon={emptyStar} color="#FFD700" />)
+            }
+        }
+        return <div>{stars}</div>
+    }
+
+    const getIcon = (activity) => {
+        const icons = {
+            souvenir : faShop,
+            visit : faPersonChalkboard,
+            campfire : faCampground,
+            music : faGuitar,
+            food: faBurger,
+            drink : faMugHot,
+            travel : faMapLocationDot,
+            swim : faPersonSwimming,
+            fishing : faFish,
+        }
+        return icons[activity] || faExclamation
+    }
+
     return ( 
+       
         <div className={cx('wrapper')}>
             {content && 
             <div className={cx('container')}>
@@ -41,9 +86,19 @@ function PlacementItem({type, data}) {
                         <div className={cx('name')}>{content.name}</div>
                         <div className={cx('reviews')}>{`${content.reviews} reviews`}</div>
                     </div>
-                    <div className={cx('star')}>{content.star}</div>
+                    <div className={cx('star')}><StarRating rating={content.star} /></div>
                     <div className={cx('description')}>{content.description}</div>
                     <Link className={cx('view-more')}><Button large>Show more</Button></Link>
+                    {
+                        content.activities && 
+                        <div className={cx('activities-container')}>
+                        {defaultActivities.map(activity => 
+                            content.activities.includes(activity) && (
+                                <FontAwesomeIcon key={activity} icon={getIcon(activity)} />
+                            )
+                        )}
+                    </div>
+                    }
                 </div>
             </div>}
         </div>
--- src/pages/Destinations/Placement/Trips/Trips.js
+++ src/pages/Destinations/Placement/Trips/Trips.js
@@ -10,10 +10,11 @@ function Trips() {
         {
             name : 'Temple of Literature',
             city : 'Ha Noi',
-            star : 4,
+            star : 3.5,
             reviews : 1000,
             img : 'https://www.indochinavoyages.com/wp-content/uploads/2019/09/temple_of_literature.jpg',
-            description : "Văn Miếu (Vietnamese: Văn Miếu, chữ Hán: 文廟[1][2]), literally translated as Temple of Literature (although a more accurate name should be Temple of Confucius, as Văn refers to Confucius), is a temple dedicated to Confucius in Hanoi, northern Vietnam. The temple was founded and first built in 1070 at the time of Emperor Lý Thánh Tông, and it hosted the Imperial Academy (Quốc Tử Giám, 國子監), Vietnam's first national university, from 1076 to 1779. In 1803, The academy was moved to the new capital of Nguyen dynasty in Hue."
+            description : "Văn Miếu (Vietnamese: Văn Miếu, chữ Hán: 文廟[1][2]), literally translated as Temple of Literature (although a more accurate name should be Temple of Confucius, as Văn refers to Confucius), is a temple dedicated to Confucius in Hanoi, northern Vietnam. The temple was founded and first built in 1070 at the time of Emperor Lý Thánh Tông, and it hosted the Imperial Academy (Quốc Tử Giám, 國子監), Vietnam's first national university, from 1076 to 1779. In 1803, The academy was moved to the new capital of Nguyen dynasty in Hue.",
+            activities : "visit,souvenir"
         },
         {
             name : 'Temple of Literature',
@@ -50,15 +51,18 @@ function Trips() {
         
 
     ]
+    
     return ( 
     <div className={cx('wrapper')}>
-        {TripItems.filter(trips => placement.replace(/([a-z])([A-Z])/g, '$1 $2') === trips.city).map((trip,index) => (
+        {TripItems.filter(trips => placement.replace(/([a-z])([A-Z])/g, '$1 $2') === trips.city).map((trip,index) => {
+            console.log(trip); 
+        return (
             <div className={cx('trips-wrapper')} key={index}>
                 <PlacementItem type={Trips.name.toLowerCase()} 
                     data = {trip}
                 />
             </div>
-        ))}  
+        )})}  
     </div>)
                 
 }
